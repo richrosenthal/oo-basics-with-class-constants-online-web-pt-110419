58c33a876721c92e49bc0495500ff42266043504
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-19 02:03:13 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,32 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS  = []
+  
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << @brand 
+    end 
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  # def brand(brand)
+  #   flag = true 
+  #   @brand = brand 
+  #   BRANDS.each do |unique| 
+  #     if unique == brand 
+  #       flag = false 
+  #     end 
+  #   end 
+    
+  #   if flag == true 
+  #   BRANDS << @brand 
+  # end 
+  # end 
 
 end
