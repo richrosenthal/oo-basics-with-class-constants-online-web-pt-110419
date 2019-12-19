31adc657186bb5c68c48e851d8fabe3f41c04f44
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-19 01:50:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS  = ["Uggs", "Rainbow", "Nike"]
+  
   def initialize(brand)
     @brand = brand
   end
@@ -10,5 +12,19 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  
+  def brand(brand)
+    flag = true 
+    @brand = brand 
+    BRANDS.each do |unique| 
+      if unique == brand 
+        flag = false 
+      end 
+    end 
+    
+    if flag == true 
+    BRANDS << @brand 
+  end 
+ end 
+BRANDS.size
 end
